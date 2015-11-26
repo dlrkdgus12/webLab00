1b14e5da397ee93de9bd06eb94251d4f659f455a
dlrkdgus12 <dlrkdgus12@naver.com>
2015-11-26 14:42:43 +0900

    Update songs_json.php

--- songs_json.php
+++ songs_json.php
@@ -17,26 +17,27 @@ if (!file_exists($SONGS_FILE)) {
 	die("ERROR 500: Server error - Unable to read input file: $SONGS_FILE");
 }
 
-header("Content-type: application/xml");
+header("Content-type: application/json");
 
-print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-print "<songs>\n";
+print "{\n  \"songs\": [\n";
 
-
+// write a code to : 
+// 1. read the "songs.txt" (or "songs_shuffled.txt" for extra mark!)
+// 2. search all the songs that are under the given top rank 
+// 3. generate the result in JSON data format 
 $lines = file($SONGS_FILE);
 for ($i = 0; $i < $top; $i++) {
 	for ($j = 0; $j < count($lines); $j++) {
 		list($title, $artist, $rank, $genre, $time) = explode("|", trim($lines[$j]));
 		if ($rank == ($i+1)) {
-			print "\t<song rank=\"$rank\">\n";
-			print "\t\t<title>$title</title>\n";
-			print "\t\t<artist>$artist</artist>\n";
-			print "\t\t<genre>$genre</genre>\n";
-			print "\t\t<time>$time</time>\n";
-			print "\t</song>\n";		
+			if ($i == ($top-1)) {
+				print "{\"rank\" : $rank, \"title\" : \"$title\", \"artist\" : \"$artist\", \"genre\" : \"$genre\", \"time\" : \"$time\"}\n";
+			} else {
+				print "{\"rank\" : $rank, \"title\" : \"$title\", \"artist\" : \"$artist\", \"genre\" : \"$genre\", \"time\" : \"$time\"},\n";
+			}
 		}
 	}
 }
-print "</songs>";
 
+print "  ]\n}\n";
 ?>
